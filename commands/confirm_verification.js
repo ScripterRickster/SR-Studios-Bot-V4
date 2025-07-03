6b0e83a61eddc29dffbbc8d2ec69b651caa162b2
Ricky Liu <101468308+ScripterRickster@users.noreply.github.com>
2025-07-03 02:00:53 +0000

    add confirm_verification.js cmd

---
+++ commands/confirm_verification.js
@@ -0,0 +1,13 @@
+// confirm_verification.js
+const { SlashCommandBuilder } = require('discord.js');
+const verifyModule = require('./verify');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('confirm_verification')
+    .setDescription('Confirm your Roblox verification after placing the code.'),
+
+  async execute(interaction) {
+    await verifyModule.confirm(interaction);
+  }
+};
